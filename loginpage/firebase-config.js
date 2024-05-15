878aa548e1bb26a90c0bda750c1185b5d799ae16
Umadsofi412 <79642369+Umadsofi412@users.noreply.github.com>
2024-05-15 13:33:05 +0530

    login page add

---
+++ loginpage/firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDGJ0JONNVg8cClxx7lSOcU59jJGD_CpIY",
+  authDomain: "portfolio-73ba2.firebaseapp.com",
+  projectId: "portfolio-73ba2",
+  storageBucket: "portfolio-73ba2.appspot.com",
+  messagingSenderId: "849741445145",
+  appId: "1:849741445145:web:ea803463f767fd83f75288",
+  measurementId: "G-9ZRD86E594",
+};
